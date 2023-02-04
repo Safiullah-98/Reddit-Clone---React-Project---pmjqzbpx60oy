c959c21a33442a0146bff6643d361fd6fae66aa1
Mohd Safiullah <102719150+Safiullah-98@users.noreply.github.com>
2023-02-04 13:26:34 +0530

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,1 +1,23 @@
+import Logo from "../../utils/logo"
+import "./Navbar.css"
+import {loginContext, postContext, userContext} from "../../App"
+import { useContext } from "react";
+
+
+export default function Navbar(){
+    const [user, setUser] = useContext(userContext);
+    let login = useContext(loginContext);
+    let post = useContext(postContext);
+
+ return(
+    <nav className="nav">
+        <Logo />
+        <div><button onClick={()=>{user? post(true) : login(true)}}>Add Post</button></div>
+         {    user?
+            <h2><i className="fa fa-user"></i>{user}</h2>:
+            <div><button onClick={()=>login(true)}>Log In</button></div> 
+         }   
+    </nav>
+ )
+}
 
